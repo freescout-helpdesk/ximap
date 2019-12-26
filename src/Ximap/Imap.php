c25904fcd6b1fe9e8140b8504d9c15271aa9677c
FreeScout <40499291+freescout-helpdesk@users.noreply.github.com>
2019-12-26 21:25:18 +0300

    Update Imap.php

--- src/Ximap/Imap.php
+++ src/Ximap/Imap.php
@@ -4,7 +4,17 @@ namespace Ximap;
 
 class Imap
 {
-    public function imap_open($mailbox , $username , $password, $options = 0, $n_retries = 0, $params = [])
+    protected $stream;
+    
+    public function __construct($mailbox, $username , $password, $options = 0, $n_retries = 0, $params = [])
+    {
+        $this->stream = self::imap_open($mailbox, $username , $password, $options, $n_retries, $params);
+    }
+    
+    /**
+     * https://www.php.net/manual/en/function.imap-open.php
+     */
+    public static function imap_open($mailbox, $username , $password, $options = 0, $n_retries = 0, $params = [])
     {
         // todo
     }
