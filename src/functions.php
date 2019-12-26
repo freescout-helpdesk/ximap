ab81b845a6547eb68b608dbed190b337abf55ba2
FreeScout <40499291+freescout-helpdesk@users.noreply.github.com>
2019-12-26 21:01:09 +0300

    Update functions.php

--- src/functions.php
+++ src/functions.php
@@ -1,4 +1,6 @@
+<?php
+
 function ximap_open($mailbox , $username , $password, $options = 0, $n_retries = 0, $params = [])
 {
-    \Ximap::imap_open($mailbox , $username , $password, $options, $n_retries, $params);
+    return Ximap\Imap::imap_open($mailbox , $username , $password, $options, $n_retries, $params);
 }
