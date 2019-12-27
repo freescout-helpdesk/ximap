cd0bb802ea1895e57089944a1496dfd2a4ca7c1b
FreeScout <40499291+freescout-helpdesk@users.noreply.github.com>
2019-12-27 09:56:09 +0300

    Update Imap.php

--- src/Ximap/Imap.php
+++ src/Ximap/Imap.php
@@ -11,7 +11,7 @@ class Imap
         $this->stream = self::imap_open($mailbox, $username , $password, $options, $n_retries, $params);
     }
 
-    public function imap_list($imap_stream, $ref, $pattern])
+    public function imap_list($imap_stream, $ref, $pattern)
     {
         return self::ximap_list($imap_stream, $ref, $pattern);
     }
@@ -21,9 +21,13 @@ class Imap
      */
     public static function ximap_open($mailbox, $username , $password, $options = 0, $n_retries = 0, $params = [])
     {
-        // todo
+        return new Ximap\Imap($mailbox, $username , $password, $options, $n_retries, $params);
     }
     
+
+    /**
+     * https://www.php.net/manual/en/function.imap-list.php
+     */
     public static function ximap_list($imap_stream, $ref, $pattern)
     {
         // todo
